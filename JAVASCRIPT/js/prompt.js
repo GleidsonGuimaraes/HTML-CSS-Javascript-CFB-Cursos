99cf33e04946fc8e39370c1ad1122ee8817f4893
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2022-05-31 10:26:15 -0300

    Aula 5

---
+++ JAVASCRIPT/js/prompt.js
@@ -0,0 +1,7 @@
+// A caixa prompt permite colher uma determinada informação e atribuir a uma variável
+
+let nome = prompt("Digite seu nome:", "Digite aqui");
+
+if(nome){
+  document.write(nome);
+}
