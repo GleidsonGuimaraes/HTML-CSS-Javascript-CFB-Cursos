dec4f19d2f0136386380d8e7906de012bea59b54
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2022-06-02 11:30:32 -0300

    Aula 24

---
+++ JAVASCRIPT/js/innerHTML.js
@@ -0,0 +1,7 @@
+let caixaTexto = document.getElementById("cxTexto");
+caixaTexto.value = "Curso de Javascript";
+
+// A propriedade innerHTML permite acessar o conteúdo contido dentro das tags do html
+let paragrafo = document.getElementById("texto");
+paragrafo.innerHTML = "Curso de Javascript";
+// alert(paragrafo);
