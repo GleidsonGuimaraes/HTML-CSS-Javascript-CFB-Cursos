17fd17aa2fedca30872644654cf720d45ad71cf5
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2022-06-02 11:10:31 -0300

    Aula 23

---
+++ JAVASCRIPT/js/getElementById.js
@@ -0,0 +1,5 @@
+document.getElementById("d1").style.background = "#F00";
+document.getElementById("d1").style.height = "100px";
+document.getElementById("d1").style.width = "200px";
+document.getElementById("d1").style.border = "5px solid #000";
+document.getElementById("d1").style.borderRadius = "10px";
