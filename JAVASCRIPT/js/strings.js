b7a70309b2135f9b13541515c5519fb70444d611
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-03-07 19:13:29 -0300

    Aulas 46 e 47 - Manipulando strings em javascript

--- JAVASCRIPT/js/strings.js
+++ JAVASCRIPT/js/strings.js
@@ -1,5 +1,3 @@
-let texto = document.getElementById("txt").innerHTML;
-
 /*
 * Método "match" para pesquisar por um determinado elemento na string. 
 * Para utilizar os modificadores é necessário indicar a string entre /(barras).
@@ -12,7 +10,34 @@ let texto = document.getElementById("txt").innerHTML;
 * Pode ser utilizado [a-b](traço) para pesquisar os elementos que estão entre os outros especificados no método.
 * As expressões podem ser mescladas [^a-b] para pesquisar todos os caracteres, menos os que estão entre a e b na string.
 * Podem ser utilizados metacaracteres. Consulte documentação sobre o método match.
+*
+* O método search mostra a posição da primeira ocorrência do elemento indicado.
+* O modificador i funciona com o método search.
+*
+* O método replace recebe uma string e modifica por outra.
+* Os modificadores i e g funcionam com o método replace. O g modifica todas as ocorrências encontradas na string.
+*
+* O método charAt retorna o caracter que ele encontrar na posição indicada dentro do método.
+* 
+* O método charCodeAt retorna o código do caracter encontrado na posição indicada no método.
+*
+* O método fromCharCode retorna o caracter referente ao código passado no método.
+*
+* O método concat faz a concatenação(junção) de uma ou mais strings.
 */
+let texto = document.getElementById("txt").innerHTML;
+let texto1 = ` Aulas 46 e 47 do curso de javascript.`;
 let res = texto.match(/[od^e]/ig);
+let res1 = texto.search(/curso/i);
+let res2 = texto.replace(/ aula 46 do curso de javascript./ig," ");
+let res3 = texto.charAt(11);
+let res4 = texto.charCodeAt(11);
+let res5 = res2.concat(texto1);
 
-document.write(`${res} : ${res.length}`);
+document.write(`Resultado do método match: ${res} : ${res.length}<br>`);
+document.write(`Resultado do método search: ${res1}<br>`);
+document.write(`Resultado do método replace: ${res2}<br>`);
+document.write(`Resultado do método charAt: ${res3}<br>`);
+document.write(`Resultado do método charCodeAt: ${res4}<br>`);
+document.write(`Resultado do método fromCharCode: ${String.fromCharCode(67)}<br>`);
+document.write(`Resultado do método concat: ${res5}<br>`);
