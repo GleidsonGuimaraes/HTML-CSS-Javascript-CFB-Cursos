0af439f85d931c48232060475b9d0ae527cb26f5
Gleidson Guimarães <gleidson.g.f@hotmail.com>
2023-03-08 18:28:00 -0300

    Aulas 46, 47 e 48 - Manipulando strings no javascript

--- JAVASCRIPT/js/strings.js
+++ JAVASCRIPT/js/strings.js
@@ -24,6 +24,23 @@
 * O método fromCharCode retorna o caracter referente ao código passado no método.
 *
 * O método concat faz a concatenação(junção) de uma ou mais strings.
+*
+* O método indexOf retorna a posição do primeiro caracter especificado que foi encontrado na string.
+* Caso não encontre nenhum, ele retorna um valor negativo.
+* O método lastIndexOf retorna a posição do último caracter encontrado na string.
+*
+* O método localeCompare retorna se as strings são iguais.
+* Se o retorno for igual a 0(zero), elas são iguais, caso contrário, se for diferente de 0 elas são diferentes.
+*
+* O método slice recorta e retorna a string especificada no método.
+* Para especificar os limites do corte, devem ser indicados as posições inicial e final, respectivamente, no método.
+*
+* O método split recorta um array contendo as strings recortadas de acordo com o caracter especificado.
+*
+* O método substr é semelhante ao slice, com a diferença que você indica o inicio do corte e quantas posições devem ser cortadas a partir daí.
+* O método substr foi descontinuado.
+*
+* Os métodos toLowerCase e toUpperCase convertem toda a string em minúscula e maiúscula, respectivamente.
 */
 let texto = document.getElementById("txt").innerHTML;
 let texto1 = ` Aulas 46 e 47 do curso de javascript.`;
@@ -33,6 +50,14 @@ let res2 = texto.replace(/ aula 46 do curso de javascript./ig," ");
 let res3 = texto.charAt(11);
 let res4 = texto.charCodeAt(11);
 let res5 = res2.concat(texto1);
+let res6 = texto.indexOf("j");
+let res7 = texto.lastIndexOf("j");
+let res8 = texto.localeCompare("texto1");
+let res9 = texto.slice(3,10);
+let res10 = texto.split(" ");
+let res11 = texto.substr(4, 6);
+let res12 = texto.toLowerCase();
+let res13 = texto.toUpperCase();
 
 document.write(`Resultado do método match: ${res} : ${res.length}<br>`);
 document.write(`Resultado do método search: ${res1}<br>`);
@@ -41,3 +66,11 @@ document.write(`Resultado do método charAt: ${res3}<br>`);
 document.write(`Resultado do método charCodeAt: ${res4}<br>`);
 document.write(`Resultado do método fromCharCode: ${String.fromCharCode(67)}<br>`);
 document.write(`Resultado do método concat: ${res5}<br>`);
+document.write(`Resultado do método indexOf: ${res6}<br>`);
+document.write(`Resultado do método lastIndexOf: ${res7}<br>`);
+document.write(`Resultado do método localeCompare: ${res8}<br>`);
+document.write(`Resultado do método slice: ${res9}<br>`);
+document.write(`Resultado do método split: Array completo: ${res10} => Posição 3 do array: ${res10[3]}<br>`);
+document.write(`Resultado do método subString: ${res11}<br>`);
+document.write(`Resultado do método toLowerCase: ${res12}<br>`);
+document.write(`Resultado do método toUpperCase: ${res13}<br>`);
